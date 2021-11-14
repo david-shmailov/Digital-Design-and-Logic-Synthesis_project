473bc44797f9ddd6667bfea62fdf081871f0f197
david <shmailov.david@gmail.com>
2021-11-15 00:02:15 +0200

    implemented encoder top

--- encoder.sv
+++ encoder.sv
@@ -1,5 +1,6 @@
 module ENC #(
-    parameters //TBD
+    parameter   MAX_CODEWORD_WIDTH = 32;
+    parameter   MAX_INFO_WIDTH=26;
 ) (
     input   rst,
             clk,
@@ -7,51 +8,49 @@ module ENC #(
             mod,
     output  data_out)
 );
+
+    localparam MAX_PARITY_WIDTH = MAX_CODEWORD_WIDTH - MAX_INFO_WIDTH;
+    // check with TA if this is ok or we somehow need to generlize it further
+    localparam info_mod_1 = 4;
+    localparam info_mod_2 = 11;
+    localparam info_mod_3 = 26;
+    localparam parity_mod_1 = 4;
+    localparam parity_mod_2 = 5;
+    localparam parity_mod_3 = 6;
+
+    localparam pad_zero_1 = MAX_CODEWORD_WIDTH - info_mod_1 - parity_mod_1;
+    localparam pad_zero_2 = MAX_CODEWORD_WIDTH - info_mod_2 - parity_mod_2;
+    localparam pad_zero_3 = MAX_CODEWORD_WIDTH - info_mod_3 - parity_mod_3;
+
+
     wire rst,clk;
-    wire [3:0] data_in;
-    reg  [7:0] data_out;
+    wire [MAX_INFO_WIDTH-1:0] data_in;
+    reg  [MAX_CODEWORD_WIDTH:0] data_out;
     wire [1:0] mod;
-    reg  [7:0] c_vector_stage1;
-    //reg  [7:0] c_vector_stage2;
 
-    wire  [7:0] c_vector_stage1_wire;
-    wire  [7:0] c_vector_stage2_wire;
+    wire  [MAX_CODEWORD_WIDTH:0] c_vector_stage1;
+    wire  [MAX_CODEWORD_WIDTH:0] c_vector_stage2;
  
-    
-
-
-    wire [7:0][3:0] H_matrix_1      = 32'hffe4_d2b1;
-    wire [3:0][3:0] right_side_H_1  = 16'hd2b1;
-    wire [3:0][3:0] left_side_H_1   = 16'hffe4;
     
     // Stage 1: computing all values of C except c[3] (C5 in the pdf)
     
-    ENC_STAGE_1 s1  (
+    ENC_STAGE_1 stage1  (
                     .clk(clk),
                     .rst(rst),
                     .data_in(data_in),
                     .mod(mod),
                     .data_out(c_vector_stage1)
-    )
+    );
 
     // Stage 2: computing c[3] (C5 in the pdf) based on all other C values
 
-    ENC_STAGE_2 s2  (
+    ENC_STAGE_2 stage2  (
                     .clk(clk),
                     .rst(rst),
                     .data_in(c_vector_stage1),
                     .mod(mod),
                     .data_out(data_out)
-    )
-
-
-    // in the following case all values of H_matrix_1[3] should be 1 with the given matricies.
-    always_comb begin : stage_2_comb
-        for(k = 0 k<7; k= k+1)begin
-            //c_vector_stage2_wire[3] = c_vector_stage2_wire[3] xor (H_matrix_1[3][k] and c_vector_stage1[k]);
-            c_vector_stage2_wire[3] = c_vector_stage2_wire[3] xor c_vector_stage1[k];
-        end
-    end
+    );
 
 
 endmodule
