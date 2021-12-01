c0a3bec8c485baa474ab3ef30c9f2e9682d94d08
david <shmailov.david@gmail.com>
2021-12-01 18:09:01 +0200

    moved to 2D output array in logic mult

--- mat_mult.sv
+++ mat_mult.sv
@@ -18,17 +18,23 @@ module MAT_MULT (
     output logic   [C_ROWS*C_COLS-1:0] C_data_out;
 
 
-    logic    [C_ROWS*C_COLS-1:0] temp;
+    logic    [C_ROWS*C_COLS-1:0] flat_temp;
+    
     logic    [A_ROWS-1:0][A_COLS-1:0] A_2D;
     logic    [B_ROWS-1:0][B_COLS-1:0] B_2D;
     logic    [C_ROWS-1:0][C_COLS-1:0] C_2D;
     genvar row;
     generate
+        //1D to 2D
         for (row = 0; row<A_ROWS ;row = row +1 ) begin
             assign A_2D[row] = A_data_in[A_COLS*row +: A_COLS -1];
         end
         for (row = 0; row<B_ROWS ;row = row +1 ) begin
             assign B_2D[row] = B_data_in[B_COLS*row +: B_COLS -1];
+        end
+        //2D to 1D
+        for (row = 0; row<C_ROWS; row = row +1) begin
+            assign flat_temp[C_COLS*row +: C_COLS-1] = C_2D[row];
         end
     endgenerate
         
@@ -43,15 +49,17 @@ module MAT_MULT (
         for (col_index = 0; col_index < C_COLS ; col_index = col_index +1 ) begin
             for (row_index = 0; row_index < C_ROWS ; row_index = row_index +1) begin
                 for (index = 0; index < A_COLS ; index = index +1) begin
-                    assign temp [row_index*C_COLS + col_index] = temp [row_index*C_COLS + col_index] ^ (A_2D[row_index][index] && B_2D[index][col_index]);
+                    assign C_2D[row_index][col_index] = C_2D[row_index][col_index] ^ (A_2D[row_index][index] && B_2D[index][col_index]);
                 end
             end
         end
     endgenerate
+
+    
     //consider transforming this into a purely combinational module
     always_ff @( posedge clk ) begin : sequencial
         if (rst) C_data_out <= 0;
-        else C_data_out <= temp;
+        else C_data_out <= flat_temp;
     end
 endmodule
 
