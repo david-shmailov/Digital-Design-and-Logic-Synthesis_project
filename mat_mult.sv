53adc04cb25726fb5c3206b9fb4ece050299aa36
david <shmailov.david@gmail.com>
2021-12-01 18:48:14 +0200

    mat mult finally compiles

--- mat_mult.sv
+++ mat_mult.sv
@@ -23,24 +23,26 @@ module MAT_MULT (
     logic    [A_ROWS-1:0][A_COLS-1:0] A_2D;
     logic    [B_ROWS-1:0][B_COLS-1:0] B_2D;
     logic    [C_ROWS-1:0][C_COLS-1:0] C_2D;
+
+    logic    [C_ROWS-1:0][C_COLS-1:0][A_COLS-1:0] sums;
+
+
     genvar row;
     generate
         //1D to 2D
-        for (row = 0; row<A_ROWS ;row = row +1 ) begin
-            assign A_2D[row] = A_data_in[A_COLS*row +: A_COLS -1];
+        for (row = 0; row< A_ROWS ;row = row +1 ) begin
+            assign A_2D[row] = A_data_in[ A_COLS*row +: A_COLS];
         end
         for (row = 0; row<B_ROWS ;row = row +1 ) begin
-            assign B_2D[row] = B_data_in[B_COLS*row +: B_COLS -1];
+            assign B_2D[row] = B_data_in[B_COLS*row +: B_COLS];
         end
         //2D to 1D
         for (row = 0; row<C_ROWS; row = row +1) begin
-            assign flat_temp[C_COLS*row +: C_COLS-1] = C_2D[row];
+            assign flat_temp[C_COLS*row +: C_COLS] = C_2D[row];
         end
     endgenerate
         
         
-
-
 
 // ask TA how to beautify this code
 
@@ -49,8 +51,9 @@ module MAT_MULT (
         for (col_index = 0; col_index < C_COLS ; col_index = col_index +1 ) begin
             for (row_index = 0; row_index < C_ROWS ; row_index = row_index +1) begin
                 for (index = 0; index < A_COLS ; index = index +1) begin
-                    assign C_2D[row_index][col_index] = C_2D[row_index][col_index] ^ (A_2D[row_index][index] && B_2D[index][col_index]);
+                    assign sums[row_index][col_index][index] = A_2D[row_index][index] && B_2D[index][col_index];
                 end
+                assign C_2D[row_index][col_index] = ^sums[row_index][col_index];
             end
         end
     endgenerate
