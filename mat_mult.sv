3e42d076cfaeeeaf9121f0b3fa456e2d6a75ef59
david <shmailov.david@gmail.com>
2021-12-01 11:59:34 +0200

    fixed syntax bit select error

--- mat_mult.sv
+++ mat_mult.sv
@@ -26,10 +26,12 @@ module MAT_MULT (
 
     always_comb begin : OneDto2D
         for (row = 0; row<A_ROWS ;row = row +1 ) begin
-            A_2D[row] = A_data_in[A_COLS*row + A_COLS -1: A_COLS*row];
+            //A_2D[row] = A_data_in[A_COLS*row + A_COLS -1: A_COLS*row];
+            A_2D[row] = A_data_in[A_COLS*row +: A_COLS -1];
         end
         for (row = 0; row<B_ROWS ;row = row +1 ) begin
-            B_2D[row] = B_data_in[B_COLS*row + B_COLS -1: B_COLS*row];
+            //B_2D[row] = B_data_in[B_COLS*row + B_COLS -1: B_COLS*row];
+            B_2D[row] = B_data_in[B_COLS*row +: B_COLS -1];
         end
     end
 
