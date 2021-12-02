2fbbd63ac293a5abb54b8843c4e7b0df975add55
david <shmailov.david@gmail.com>
2021-12-02 15:36:15 +0200

    finally fixed that fucking segmentation bug

--- dec_check.sv
+++ dec_check.sv
@@ -24,12 +24,12 @@ module DEC_CHK (
     localparam pad_zero_2 = MAX_CODEWORD_WIDTH - full_length_mod_2;
     localparam pad_zero_3 = MAX_CODEWORD_WIDTH - full_length_mod_3;
 
-    input logic    rst,clk;
-    input logic    [MAX_CODEWORD_WIDTH-1:0]     data_in;
-    input logic    [MAX_PARITY_WIDTH-1:0]        s_vector;
-    input logic    [1:0]                         work_mod;
-    output logic    [MAX_CODEWORD_WIDTH-1:0]    data_out;
-    output logic    [1:0]                       num_of_errors;
+    input   logic                               rst,clk;
+    input   logic   [MAX_CODEWORD_WIDTH-1:0]    data_in;
+    input   logic   [MAX_PARITY_WIDTH-1:0]      s_vector;
+    input   logic   [1:0]                       work_mod;
+    output  logic   [MAX_CODEWORD_WIDTH-1:0]    data_out;
+    output  logic   [1:0]                       num_of_errors;
 
 
     logic   [MAX_PARITY_WIDTH-1:0][MAX_CODEWORD_WIDTH-1:0]     H_matrix_1;
@@ -40,6 +40,11 @@ module DEC_CHK (
     logic   [MAX_CODEWORD_WIDTH-1:0][MAX_PARITY_WIDTH-1:0]     H_2_transpose;
     logic   [MAX_CODEWORD_WIDTH-1:0][MAX_PARITY_WIDTH-1:0]     H_3_transpose;
 
+    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_1, correction_vector_mod_2, correction_vector_mod_3; // we need full codeword width for the xor with data_in later
+    logic     eq_to_col;
+    logic     [MAX_CODEWORD_WIDTH-1:0]    temp_out;
+
+
     assign H_matrix_1 = 192'hFF_0000_00E4_0000_00D2_0000_00B1; // i assume MSB bits will be zero padded
     assign H_matrix_2 = 192'hFFFF_0000_FE08_0000_F1C4_0000_CDA2_0000_AB61;
     assign H_matrix_3 = 192'hFFFF_FFFF_FFFE_0010_FF01_FC08_F0F1_E384_CCCD_9B42_AAAB_56C1;
@@ -47,7 +52,7 @@ module DEC_CHK (
 
 
     genvar row,col;    
-    for (row = 0; row < 6 ; row = row +1 ) begin
+    for (row = 0; row < MAX_PARITY_WIDTH ; row = row +1 ) begin
         for (col = 0; col < MAX_CODEWORD_WIDTH ; col = col +1) begin
             assign H_1_transpose [col][row] = H_matrix_1 [row][col];
             assign H_2_transpose [col][row] = H_matrix_2 [row][col];
@@ -58,56 +63,54 @@ module DEC_CHK (
 
 
     
-    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_1, correction_vector_mod_2, correction_vector_mod_3;
-
-    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_1_sample, correction_vector_mod_2_sample, correction_vector_mod_3_sample;
-
-
-    logic     eq_to_col;
-    logic     [MAX_CODEWORD_WIDTH-1:0]    temp_out;
+    
 
 
     // assumption: unspecified bits are zero for correction_vector_mod_#
     genvar i;
     generate
         for (i=0; i<full_length_mod_1; i=i+1) begin // need to full work_mod 1 so it doesnt check equality with zero padding
-            assign correction_vector_mod_1[i] = H_1_transpose[i] == s_vector; // this checks if they are equal 
+            assign correction_vector_mod_1[i] = (H_1_transpose[i] == s_vector) ? 1'b1 : 1'b0; // this checks if they are equal 
         end
+        assign correction_vector_mod_1 [MAX_CODEWORD_WIDTH-1 : full_length_mod_1] = {pad_zero_1{1'b0}};
+
         for (i=0; i<full_length_mod_2; i=i+1) begin
-            assign correction_vector_mod_2[i] = H_2_transpose[i] == s_vector; // this checks if they are equal
+            assign correction_vector_mod_2[i] = (H_2_transpose[i] == s_vector) ? 1'b1 : 1'b0; // this checks if they are equal
         end
+        assign correction_vector_mod_2 [MAX_CODEWORD_WIDTH-1 : full_length_mod_2] = {pad_zero_2{1'b0}};
+
         for (i=0; i<full_length_mod_3; i=i+1) begin
-            assign correction_vector_mod_3[i] = H_3_transpose[i] == s_vector; // this checks if they are equal
+            assign correction_vector_mod_3[i] = (H_3_transpose[i] == s_vector) ? 1'b1 : 1'b0; // this checks if they are equal
         end
     endgenerate
 
-    always_ff @( posedge clk or negedge rst) begin : after_CV_compute
-        if (!rst) begin
-            correction_vector_mod_1_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
-            correction_vector_mod_2_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
-            correction_vector_mod_3_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
-        end else begin
-            correction_vector_mod_1_sample <= correction_vector_mod_1;
-            correction_vector_mod_2_sample <= correction_vector_mod_2;
-            correction_vector_mod_3_sample <= correction_vector_mod_3;
-        end
-    end
+    // always_ff @( posedge clk) begin : after_CV_compute
+    //     if (!rst) begin
+    //         correction_vector_mod_1_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
+    //         correction_vector_mod_2_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
+    //         correction_vector_mod_3_sample <= {MAX_CODEWORD_WIDTH{1'b0}};
+    //     end else begin
+    //         correction_vector_mod_1_sample <= correction_vector_mod_1;
+    //         correction_vector_mod_2_sample <= correction_vector_mod_2;
+    //         correction_vector_mod_3_sample <= correction_vector_mod_3;
+    //     end
+    // end
 
 
 
     always_comb begin : reduce
         case(work_mod)
             2'b00   :   begin
-                        eq_to_col = |correction_vector_mod_1_sample;
-                        temp_out   = data_in ^ correction_vector_mod_1_sample; //correct the data (XOR)
+                        eq_to_col = |correction_vector_mod_1;
+                        temp_out   = data_in ^ correction_vector_mod_1; //correct the data (XOR)
             end
             2'b01   :   begin
-                        eq_to_col = |correction_vector_mod_2_sample;
-                        temp_out   = data_in ^ correction_vector_mod_2_sample; //correct the data
+                        eq_to_col = |correction_vector_mod_2;
+                        temp_out   = data_in ^ correction_vector_mod_2; //correct the data
             end
             2'b10   :   begin
-                        eq_to_col = |correction_vector_mod_3_sample;
-                        temp_out   = data_in ^ correction_vector_mod_3_sample; //correct the data
+                        eq_to_col = |correction_vector_mod_3;
+                        temp_out   = data_in ^ correction_vector_mod_3; //correct the data
             end
             default :   begin
                         eq_to_col = 1'b0;
@@ -117,7 +120,7 @@ module DEC_CHK (
     end
 
     
-    always_ff @( posedge clk or negedge rst) begin : DEC_outputs
+    always_ff @( posedge clk) begin : DEC_outputs
         if (!rst) begin
             num_of_errors <= 2'b00;
             data_out <= {MAX_CODEWORD_WIDTH{1'b0}};
