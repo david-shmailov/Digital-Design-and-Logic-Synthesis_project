39c0891f44a31886285d7d2eea29b9220c9c3524
david <shmailov.david@gmail.com>
2021-12-02 15:59:30 +0200

    removed matrix zero columns on left side

--- dec_check.sv
+++ dec_check.sv
@@ -32,34 +32,42 @@ module DEC_CHK (
     output  logic   [1:0]                       num_of_errors;
 
 
-    logic   [MAX_PARITY_WIDTH-1:0][MAX_CODEWORD_WIDTH-1:0]     H_matrix_1;
-    logic   [MAX_PARITY_WIDTH-1:0][MAX_CODEWORD_WIDTH-1:0]     H_matrix_2;
-    logic   [MAX_PARITY_WIDTH-1:0][MAX_CODEWORD_WIDTH-1:0]     H_matrix_3;
+    logic   [MAX_PARITY_WIDTH-1:0][full_length_mod_1-1:0]     H_matrix_1;
+    logic   [MAX_PARITY_WIDTH-1:0][full_length_mod_2-1:0]     H_matrix_2;
+    logic   [MAX_PARITY_WIDTH-1:0][full_length_mod_3-1:0]     H_matrix_3;
 
-    logic   [MAX_CODEWORD_WIDTH-1:0][MAX_PARITY_WIDTH-1:0]     H_1_transpose;
-    logic   [MAX_CODEWORD_WIDTH-1:0][MAX_PARITY_WIDTH-1:0]     H_2_transpose;
-    logic   [MAX_CODEWORD_WIDTH-1:0][MAX_PARITY_WIDTH-1:0]     H_3_transpose;
+    logic   [full_length_mod_1-1:0][MAX_PARITY_WIDTH-1:0]     H_1_transpose;
+    logic   [full_length_mod_2-1:0][MAX_PARITY_WIDTH-1:0]     H_2_transpose;
+    logic   [full_length_mod_3-1:0][MAX_PARITY_WIDTH-1:0]     H_3_transpose;
 
-    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_1, correction_vector_mod_2, correction_vector_mod_3; // we need full codeword width for the xor with data_in later
+    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_1;
+    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_2;
+    logic     [MAX_CODEWORD_WIDTH-1:0]    correction_vector_mod_3;
     logic     eq_to_col;
     logic     [MAX_CODEWORD_WIDTH-1:0]    temp_out;
 
 
-    assign H_matrix_1 = 192'hFF_0000_00E4_0000_00D2_0000_00B1; // i assume MSB bits will be zero padded
-    assign H_matrix_2 = 192'hFFFF_0000_FE08_0000_F1C4_0000_CDA2_0000_AB61;
+    assign H_matrix_1 = 48'hFFE4_D2B1; // i assume MSB bits will be zero padded
+    assign H_matrix_2 = 96'hFFFF_FE08_F1C4_CDA2_AB61;
     assign H_matrix_3 = 192'hFFFF_FFFF_FFFE_0010_FF01_FC08_F0F1_E384_CCCD_9B42_AAAB_56C1;
 
 
 
-    genvar row,col;    
+    genvar row,col; 
+    // in an attempt to discard useless zeros from the matrix we did that:
+    generate   
     for (row = 0; row < MAX_PARITY_WIDTH ; row = row +1 ) begin
-        for (col = 0; col < MAX_CODEWORD_WIDTH ; col = col +1) begin
+        for (col = 0; col < full_length_mod_1 ; col = col +1) begin
             assign H_1_transpose [col][row] = H_matrix_1 [row][col];
-            assign H_2_transpose [col][row] = H_matrix_2 [row][col];
-            assign H_3_transpose [col][row] = H_matrix_3 [row][col];
         end
+        for (col = 0; col < full_length_mod_2 ; col = col +1) begin
+            assign H_2_transpose [col][row] = H_matrix_2 [row][col];
+        end
+        for (col = 0; col < full_length_mod_3 ; col = col +1) begin
+            assign H_3_transpose [col][row] = H_matrix_3 [row][col];
+        end     
     end
-
+    endgenerate
 
 
     
