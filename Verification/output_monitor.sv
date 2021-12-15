9be7b1ee578719fa8a2fe97bae13f899c9f6c5c9
AFRIATOH <afriat1991@gmail.com>
2021-12-15 15:30:23 +0200

    output monitor finished(almost)

--- Verification/output_monitor.sv
+++ Verification/output_monitor.sv
@@ -10,12 +10,23 @@ class out_monitor;
     mailbox mon2chk;
 
     function new(mailbox mon2chk,virtual intf.MONITOR inter);
-
-        this.mon2chk = new();
+        this.mon2chk = mon2chk;
         this.inter = inter;
         
     endfunction //new()
 
+
+
+    task sampling;
+        forever begin
+            transaction trans = new;
+            @(posedge inter.clk)
+            trans.data_out <= inter.data_out;
+            trans.operation_done <=inter.operation_done;
+            trans.num_of_errors <= inter.num_of_errors;
+            mon2chk.put(trans);
+        end
+    endtask 
 
 
 endclass //Out_monitor
