d69d6dc1044b3c93ab455dbc4ab94f8df61987f1
AFRIATOH <afriat1991@gmail.com>
2021-12-15 21:41:53 +0200

    gimi

--- Verification/test_transaction.sv
+++ Verification/test_transaction.sv
@@ -1,26 +1,33 @@
 class rand_trans;
-   parameter       AMBA_WORD = 32;
-   parameter       AMBA_ADDR_WIDTH = 20;
-   parameter       DATA_WIDTH = 32;
-   
 
-   rand bit [AMBA_WORD-1:0] ctrl;
-   rand bit [AMBA_WORD-1:0] data_in;
-   rand bit [AMBA_WORD-1:0] codeword_width;
-   rand bit [AMBA_WORD-1:0] noise;
-   
-   constraint c_ctrl {
-      ctrl >= 0;
-      ctrl <= 2; 
-   }
-  
-  constraint c_codewidth {
-    	codeword_width >= 0;
-    	codeword_width <= 0; 
+  parameter       AMBA_WORD = 32;
+  parameter       AMBA_ADDR_WIDTH = 20;
+  parameter       DATA_WIDTH = 32; // codeword width max
+    
+
+  rand bit [AMBA_WORD-1:0] ctrl;
+  rand bit [AMBA_WORD-1:0] data_in;
+  rand bit [AMBA_WORD-1:0] codeword_width;
+  rand bit [AMBA_WORD-1:0] noise;
+    
+  constraint c_ctrl
+  {
+    ctrl >= 0;
+    ctrl <= 2; 
   }
-  constraint c_data_in{
-    data_in>=0;
-    data_in<=16;
+    
+  constraint c_codewidth
+  {
+    
+  }
+  constraint c_data_in
+  {
+      
+  }
+
+  constraint c_noise
+  {
+    
   }
 
 endclass
