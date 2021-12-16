e1bb7ca4ebd3b5621f153a6fcdf273ab0d6f6b46
AFRIATOH <afriat1991@gmail.com>
2021-12-16 20:05:33 +0200

    constrain might be done

--- Verification/test_transaction.sv
+++ Verification/test_transaction.sv
@@ -9,6 +9,7 @@ class apb_trans;
   rand bit [AMBA_WORD-1:0] data_in;
   rand bit [AMBA_WORD-1:0] codeword_width;
   rand bit [AMBA_WORD-1:0] noise;
+  rand bit [AMBA_WORD-1:0] noise_tri;
     
   constraint c_ctrl
   {
@@ -16,18 +17,58 @@ class apb_trans;
     ctrl <= 2; 
   }
     
-  constraint c_codewidth
+  constraint c_codewidth  //not sure to include DATA_WIDTH PARAMETER
   {
-    
+    codeword_width >= 0;
+    codeword_width <= 2; 
   }
   constraint c_data_in
   {
-      
+    data_in >= 0;
+    if(ctrl == 1) begin
+      case (codeword_width):
+
+        0: data_in <= 256;
+              
+        1: data_in <= 65,536;
+
+        2: data_in <= 4,294,967,296;
+          
+        default: data_in <= 256;
+      endcase
+    end 
+    else begin
+      case (codeword_width):
+
+        0: data_in <= 16;
+              
+        1: data_in <= 2,048;
+
+        2: data_in <= 67,108,864;
+          
+        default: data_in <= 16;
+      endcase
+    end
   }
+
+ constraint c_noise_tri 
+ {
+    noise_tri >= 0;
+    noise_tri <= 2; 
+ }
+
 
   constraint c_noise
   {
-    
+    case (noise_tri): 
+      0: noise = 0;
+
+      1: $countones(noise) == 1;
+
+      2: $countones(noise) == 2;
+
+      default: noise = 0
+    endcase
   }
 
 endclass
