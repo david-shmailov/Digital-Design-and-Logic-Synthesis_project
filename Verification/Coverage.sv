c16145bf692ef1431edafa60fe3838054490427c
AFRIATOH <afriat1991@gmail.com>
2021-12-18 17:05:54 +0200

    Pull gimi

--- Verification/Coverage.sv
+++ Verification/Coverage.sv
@@ -4,33 +4,40 @@ class Functional_Coverage;
    parameter       AMBA_ADDR_WIDTH = 20;
    parameter       DATA_WIDTH = 32;
 
+
+
     virtual intf.MONITOR inter;
 
     //test for inputs and outputs of the DUT
-    covergroup inDUT () @(posedge inter.clk && inter.PEANBLE);
+    covergroup inDUT () @(posedge inter.clk && inter.PEANBLE && negedge inter.rst);
 
-        cover4ctrl : coverpoint inter.PWDATA iff(!inter.rst && (inter.PADDR == 0)) ;{
+        cover4ctrl : coverpoint inter.PWDATA iff(inter.PADDR == 0) ;{
 
             bins encoder_mode = {0};
             bins decoder_mode = {1};
             bins full_channel = {2};
         }
 
-        cover4data_in : coverpoint inter.PWDATA iff(!inter.rst && (inter.PADDR == 'h4)) ;{
+        cover4data_in : coverpoint inter.PWDATA iff(inter.PADDR == 'h4) ;
 
+        cover4codeword_width : coverpoint inter.PWDATA iff(inter.PADDR == 'h8) ;{
+
+            bins bit8 = {0};
+            bins bit16 = {1};
+            bins bit32 = {2};
         }
 
-        cover4codeword_width : coverpoint inter.PWDATA iff(!inter.rst && (inter.PADDR == 'h8)) ;{
-
-            bins 8bit = {0};
-            bins 16bit = {1};
-            bins 32bit = {2};
+        cover4ZEROnoise : coverpoint inter.PWDATA iff(inter.PADDR == 'hc && inter.PWDATA == 0) ;{
+            bins ZeroNoise = {0};
+        }
+        cover4ONEnoise : coverpoint (^inter.PWDATA) iff(inter.PADDR == 'hc !(inter.PWDATA == 0)) ;{
+            bins OneNoise = {1};
+        }
+        cover4TWOnoise : coverpoint (^inter.PWDATA) iff(inter.PADDR == 'hc !(inter.PWDATA == 0)) ;{
+            bins TwoNoise = {2};
         }
 
-        cover4noise : coverpoint inter.PWDATA iff(!inter.rst && (inter.PADDR == 'hc)) ;{
 
-        }        
-        
     endgroup
 
     
