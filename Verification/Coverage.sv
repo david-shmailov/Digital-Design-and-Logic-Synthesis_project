e19731a5916c17ed7a17d07caf2c768f11a82d69
AFRIATOH <afriat1991@gmail.com>
2021-12-19 21:33:41 +0200

    coverage update

--- Verification/Coverage.sv
+++ Verification/Coverage.sv
@@ -9,46 +9,56 @@ class coverage;
     virtual intf.MONITOR inter;
 
     //test for inputs and outputs of the DUT
-    covergroup inDUT () @(posedge inter.clk && inter.PEANBLE && negedge inter.rst);
+    covergroup inOutDUT () @(posedge inter.clk  && negedge inter.rst);
 
-        cover4ctrl : coverpoint inter.PWDATA iff(inter.PADDR == 0) {
+        cover4ctrl : coverpoint inter.PWDATA iff(inter.PADDR == 0 && inter.PEANBLE) {
 
             bins encoder_mode = {0};
             bins decoder_mode = {1};
             bins full_channel = {2};
+            modeXnoise cross cover4ctrl,cover4ZEROnoise;
         }
 
-        cover4data_in : coverpoint inter.PWDATA iff(inter.PADDR == 'h4) ;
-
-        cover4codeword_width : coverpoint inter.PWDATA iff(inter.PADDR == 'h8) {
+        cover4codeword_width : coverpoint inter.PWDATA iff(inter.PADDR == 'h8 && inter.PEANBLE) {
 
             bins bit8 = {0};
             bins bit16 = {1};
             bins bit32 = {2};
         }
 
-        cover4ZEROnoise : coverpoint inter.PWDATA iff(inter.PADDR == 'hc && inter.PWDATA == 0) {
+        cover4data_in : coverpoint inter.PWDATA iff(inter.PADDR == 'h4 && inter.PEANBLE) {
+            bins DataIn = {[0,AMBA_WORD]};
+            sizeXdataIn cross DataIn,cover4codeword_width;
+            modeXDataIn cross DataIn,cover4ctrl;
+        }
+
+        cover4ZEROnoise : coverpoint inter.PWDATA iff(inter.PADDR == 'hc && inter.PWDATA == 0 && inter.PEANBLE) {
             bins ZeroNoise = {0};
         }
+
         cover4ONEnoise : coverpoint (^inter.PWDATA) iff(inter.PADDR == 'hc !(inter.PWDATA == 0)) {
             bins OneNoise = {1};
         }
+
         cover4TWOnoise : coverpoint (^inter.PWDATA) iff(inter.PADDR == 'hc !(inter.PWDATA == 0)) {
             bins TwoNoise = {0};
         }
 
+        cover4data_out : coverpoint inter.operation_done {
+            bins DataOut = {[0,AMBA_WORD]};
+            sizeXdataOut cross DataOut,cover4codeword_width;
+            modeXDataIn cross DataOut,cover4ctrl;
+        }
+
+        cover4NumberOfErrors : coverpoint inter.operation_done {
+            bins NumberOfErrors = {[0,2]};
+            noiseErrros cross NumberOfErrors,cover4ZEROnoise;
+        }
 
     endgroup
+    
+    
 
-    
-    covergroup outDUT () @(posedge inter.operation_done);
-
-
-    endgroup
-    
-    
-    
-    
     function new(virtual intf.MONITOR inter);
 
         inDUT = new;
